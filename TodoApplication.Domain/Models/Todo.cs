cb16b59db9d81495d4598faf74c97854a5cc6151
mark.kelemen <mark.kelemen@pwc.com>
2023-09-29 00:04:41 +0200

    TODO1: refact

--- TodoApplication.Domain/Models/Todo.cs
+++ TodoApplication.Domain/Models/Todo.cs
@@ -2,7 +2,6 @@
 
 public sealed class Todo : Entity
 {
-    public Guid Id { get; set; }
     public required string Title { get; set; }
     public string? Descreption { get; set; }
     public PriorityLevel Priority { get; set; }
