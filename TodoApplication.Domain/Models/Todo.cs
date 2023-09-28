e6a5070bb1a8579ffab8d97ec1ea0b6420c75eb3
mark.kelemen <mark.kelemen@pwc.com>
2023-09-29 00:00:22 +0200

    TODO1: todo updated

--- TodoApplication.Domain/Models/Todo.cs
+++ TodoApplication.Domain/Models/Todo.cs
@@ -1,8 +1,8 @@
 ﻿namespace TodoApplication.Domain;
 
-public sealed class Todo
+public sealed class Todo : Entity
 {
-    public long Id { get; set; }
+    public Guid Id { get; set; }
     public required string Title { get; set; }
     public string? Descreption { get; set; }
     public PriorityLevel Priority { get; set; }
