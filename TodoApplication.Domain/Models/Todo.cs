6cde03eb3f23f50031d2084a0aea92c1779aa065
mark.kelemen <mark.kelemen@pwc.com>
2023-09-28 23:55:20 +0200

    TODO1: todo object created

---
+++ TodoApplication.Domain/Models/Todo.cs
@@ -0,0 +1,9 @@
+﻿namespace TodoApplication.Domain;
+
+public sealed class Todo
+{
+    public long Id { get; set; }
+    public required string Title { get; set; }
+    public string? Descreption { get; set; }
+    public PriorityLevel Priority { get; set; }
+}
